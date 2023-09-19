72cdd85aa1dd8723367c8666075b511a1f4bb842
Jaimedv17 <jaime.durand.vega@gmail.com>
2023-09-18 21:35:28 -0500

    ejercicio terminado

---
+++ app.js
@@ -0,0 +1,73 @@
+
+// // 1.Que es una funcion y para que sirve
+
+// //Es un un conjunto de tareas y sirve para crear he invocar streng o numeros para realizar operaciones.
+
+
+function sumar () {
+    console.log (`FrontEnd`)
+}
+
+sumar();
+
+function paquito() {
+    console.log(100- 2);
+  }
+
+paquito();
+
+
+// 2.Que es una funcion por declaracion y expresion y en que se diferencian
+
+//Sirven para definir funsiones, y se diferencias sobre la posicion en la que se declaran, funsion de declaracion se puede usar antes de ser declarada y expresion no.
+
+
+function parImpar(namber){
+    if (namber % 2 === 0){
+        return "Par";
+    }
+    else {
+        return "Impar"
+    }
+}
+
+console.log (parImpar(5))
+console.log (parImpar(20))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
